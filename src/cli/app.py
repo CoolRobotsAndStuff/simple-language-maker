b1f1b093606f0b858df4b1525ea940f19331cf62
ale <aledeum.saf@gmail.com>
2023-09-15 18:19:25 -0300

    Finished new language page and translator page

--- src/cli/app.py
+++ src/cli/app.py
@@ -5,7 +5,7 @@ import sys
 import random
 import textwrap
 
-from asciimatics.widgets import Frame, Layout, Divider, Button, TextBox, Widget, VerticalDivider
+from asciimatics.widgets import Frame, Layout, Divider, Button, TextBox, Widget, VerticalDivider, ListBox
 from asciimatics.scene import Scene
 from asciimatics.renderers import SpeechBubble
 from asciimatics.effects import Print
@@ -41,22 +41,74 @@ my_palette = defaultdict(
 
 class TranslatorModel:
     def __init__(self) -> None:
-        self.current_language = internal.make_random_language()
-        self.language_pack = spanish(self.current_language)
+        self.current_generated_language = internal.make_random_language()
+        self.current_loaded_language = internal.SimpleLanguage("???", [])
+        self.language_pack = spanish(self.current_generated_language, self.current_loaded_language)
         tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
 
     def set_random_language(self):
-        self.current_language = internal.make_random_language()
-        self.language_pack = spanish(self.current_language)
+        self.current_generated_language = internal.make_random_language()
+        self.language_pack = spanish(self.current_generated_language, self.current_loaded_language)
 
     def save_current_language(self):
-        folder_path = filedialog.askdirectory()
-        internal.save_to_file(Path(folder_path), self.current_language)
+        file_name = self.current_generated_language.name + internal.FILE_EXTENSION
+        file_path = filedialog.asksaveasfilename(defaultextension=internal.FILE_EXTENSION, initialfile=file_name)
+        try:
+            if isinstance(file_path, str):
+                internal.save_to_file(Path(file_path), self.current_generated_language)
+        except IsADirectoryError:
+            pass
     
     def open_language_from_file(self):
-        file = filedialog.askopenfilename(defaultextension=internal.FILE_EXTENSION, filetypes=[internal.FILE_EXTENSION])
-        self.current_language = internal.open_from_file(Path(file))
-    
+        file = filedialog.askopenfilename(defaultextension=internal.FILE_EXTENSION)
+        try:
+            if isinstance(file, str):
+                self.current_loaded_language = internal.open_from_file(Path(file))
+                self.language_pack = spanish(self.current_generated_language, self.current_loaded_language)
+        except IsADirectoryError:
+            pass
+
+    def translate_file(self):
+        input_file_name = filedialog.askopenfilename(defaultextension=".txt")
+        try:
+            if isinstance(input_file_name, str):
+                input_file_name = Path(input_file_name)
+                with open(input_file_name, "r") as input_file:
+                    input_text = input_file.readlines()
+
+                new_file_name, extension = input_file_name.name.split(".")
+
+                new_file_name += f"_{self.language_pack['file']['translated_to']}_{self.current_loaded_language.name}.{extension}"
+
+                fonetized = internal.fonetize("\n".join(input_text))
+                translated = internal.translate(fonetized, self.current_loaded_language)
+                output_file_name = filedialog.asksaveasfilename(defaultextension=extension, initialfile=new_file_name)
+                if isinstance(output_file_name, str):
+                    with open(output_file_name, "w") as output_file:
+                        output_file.write(translated)
+        except IsADirectoryError:
+            pass
+
+    def detranslate_file(self):
+        try:
+            input_file_name = filedialog.askopenfilename(defaultextension=".txt")
+            if isinstance(input_file_name, str):
+                input_file_name = Path(input_file_name)
+                with open(input_file_name, "r") as input_file:
+                    input_text = input_file.read()
+
+                new_file_name, extension = input_file_name.name.split(".")
+
+                new_file_name += f"_{self.language_pack['file']['translated_to']}_{self.language_pack['file']['nat_lang_name']}.{extension}"
+
+                translated = internal.detranslate(input_text, self.current_loaded_language)
+                output_file_name = filedialog.asksaveasfilename(defaultextension=extension, initialfile=new_file_name)
+                if isinstance(output_file_name, str):
+                    with open(output_file_name, "w") as output_file:
+                        output_file.write(translated)
+        except IsADirectoryError:
+            pass
+
 class TabButtons(Layout):
     def __init__(self, frame: Frame, active_tab_idx):
         cols = [1, 1, 1]
@@ -180,7 +232,7 @@ class NewLanguageUIFrame(Frame):
 
     def on_text_edit(self):
         fonetized = internal.fonetize("\n".join(self.input_text.value))
-        self.translation_text.value = internal.translate(fonetized, self._model.current_language).split("\n")
+        self.translation_text.value = internal.translate(fonetized, self._model.current_generated_language).split("\n")
 
     def new_language(self):
         self._model.set_random_language()
@@ -206,21 +258,39 @@ class TranslatorUIFrame(Frame):
         self.my_screen = screen
 
 
+        self.palette = my_palette
+
+        self.my_screen = screen
+
+
         title_layout = Layout([1])
         self.add_layout(title_layout)
         self.title_box = WrappedTextBox(1, line_wrap=True, readonly=True, justify='center')
         self.title_box.value = self._model.language_pack["translator"]["title"]
         self.title_box.disabled = True
 
-
-        instructions_box = WrappedTextBox(4, line_wrap=True, readonly=True, justify='left')
-        instructions_box.value = self._model.language_pack["translator"]["instructions"]
-        instructions_box.disabled = True
-
+        self.instructions_box = WrappedTextBox(6, line_wrap=True, readonly=True, justify='left')
+        self.instructions_box.value = self._model.language_pack["translator"]["instructions"]
+        self.instructions_box.disabled = True
 
         title_layout.add_widget(self.title_box)
         title_layout.add_widget(Divider())
-        title_layout.add_widget(instructions_box)
+        title_layout.add_widget(self.instructions_box)
+        title_layout.add_widget(Divider())
+
+
+        direction_chooser_layout = Layout([1, 2, 2, 1])
+        self.add_layout(direction_chooser_layout)
+        label = WrappedTextBox(3, line_wrap=True, readonly=True, justify='right')
+        label.value = self._model.language_pack["translator"]["translation_direction"]
+        label.disabled = True
+        direction_chooser_layout.add_widget(label, 1)
+
+        self.language_selector = ListBox(3, [(self._model.language_pack["translator"]["nat_lang_to_gen"], 1), \
+                                             (self._model.language_pack["translator"]["gen_lang_to_nat"], 2)], \
+                                              centre=True, on_change=self.on_text_edit)
+        
+        direction_chooser_layout.add_widget(self.language_selector, 2)
 
         divider_layout15 = Layout([1])
         self.add_layout(divider_layout15)
@@ -236,7 +306,7 @@ class TranslatorUIFrame(Frame):
         translator_layout.add_widget(VerticalDivider(), 1)
 
         self.input_text = WrappedTextBox(Widget.FILL_COLUMN, line_wrap=True, on_change=self.on_text_edit)
-        self.input_text.value = self._model.language_pack["new_language"]["test_poetry"]
+        self.input_text.value = self._model.language_pack["translator"]["text_hint"]
         translator_layout.add_widget(self.input_text, 0)
 
         divider_layout2 = Layout([1])
@@ -245,31 +315,55 @@ class TranslatorUIFrame(Frame):
 
         translator_buttons_layout = Layout([1, 1])
         self.add_layout(translator_buttons_layout)
-        translator_buttons_layout.add_widget(Button(self._model.language_pack["new_language"]["new_button"][0], on_click=self.new_language), 0)
-        translator_buttons_layout.add_widget(Button(self._model.language_pack["new_language"]["save_button"][0], on_click=self._model.save_current_language), 1)
+        translator_buttons_layout.add_widget(Button(self._model.language_pack["translator"]["load_lang_button"], on_click=self.open_language), 0)
+        translator_buttons_layout.add_widget(Button(self._model.language_pack["translator"]["translate_file_button"], on_click=self.translate_file), 1)
 
         divider_layout3 = Layout([1])
         self.add_layout(divider_layout3)
         divider_layout3.add_widget(Divider())
 
-
-        layout2 = TabButtons(self, 2)
-        self.add_layout(layout2)
+        tab_buttons_layout = TabButtons(self, 2)
+        self.add_layout(tab_buttons_layout)
         self.fix()
+
+    def on_text_edit(self):
+        if self.language_selector.value == 1:
+            fonetized = internal.fonetize("\n".join(self.input_text.value))
+            self.translation_text.value = internal.translate(fonetized, self._model.current_loaded_language).split("\n")
+        else:
+            self.translation_text.value = internal.detranslate("\n".join(self.input_text.value), self._model.current_loaded_language).split("\n")
+
+    def open_language(self):
+        self._model.open_language_from_file()
+        self.on_text_edit()
+
+
+        self.language_selector.options = [(self._model.language_pack["translator"]["nat_lang_to_gen"], 1), \
+                                          (self._model.language_pack["translator"]["gen_lang_to_nat"], 2)]
+        
+        self.title_box.value = self._model.language_pack["translator"]["title"]
+        self.instructions_box.value = self._model.language_pack["translator"]["instructions"]
+
+
+    def translate_file(self):
+        if self.language_selector.value == 1:
+            self._model.translate_file()
+        else:
+            self._model.detranslate_file()
+
 
 
 class App():
     def __init__(self) -> None:
         self.last_scene = None
         self.model = TranslatorModel()
-        self.language_pack = spanish(self.model.current_language)
 
     def demo(self, screen, scene):
         
         scenes = [
-            HomeScene(screen, self.language_pack, name="Tab1"),
+            HomeScene(screen, self.model.language_pack, name="Tab1"),
             NewLanguageScene(screen, self.model, name="Tab2"),
-            Scene([BravoPage(screen)], -1, name="Tab3"),
+            Scene([TranslatorUIFrame(screen, self.model, 0)], -1, name="Tab3"),
         ]
         screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
 
--- src/cli/languages.py
+++ src/cli/languages.py
@@ -2,42 +2,63 @@ import random
 from src import internal
 
 
-def spanish(generated_language: internal.SimpleLanguage):
+def spanish(generated_language: internal.SimpleLanguage, loaded_language: internal.SimpleLanguage):
 
     word_for_language = random.choice(('Idiomas', 'Lenguajes', 'Lenguas'))
     app_name = internal.fonetize(f'Creador de {word_for_language} Simple')
 
     languaje_adjective = random.choice(("increíble", "hermoso", "curioso", "complejo", "interesante", "harmonioso", "bello", "difícil"))
 
-    return  {"home":          {"title": [f"¡Bienvenido al Creador de Idiomas Simple! o '{internal.translate(app_name, generated_language)}' en el idioma '{generated_language.name}'"],
-                                 "instructions": ['¿Que deseas hacer?', 
-                                                  ' ', 
-                                                  '* Para crear un nuevo idioma presiona «Nuevo»',
-                                                  '* Para cargar un idioma desde un archivo y traducir presiona «Traducir»',
-                                                  '* Para salir... bueno, presiona «Salir» xd']},
+    return  {   "home":          {  "title":        [f"¡Bienvenido al Creador de Idiomas Simple! o '{internal.translate(app_name, generated_language)}' en el idioma '{generated_language.name}'"],
+                                    "instructions": ['¿Que deseas hacer?', 
+                                                    ' ', 
+                                                    '* Para crear un nuevo idioma presiona «Nuevo»',
+                                                    '* Para cargar un idioma desde un archivo y traducir presiona «Traducir»',
+                                                    '* Para salir... bueno, presiona «Salir» xd']},
 
-               "new_language":  {"title": [f'Observa el {languaje_adjective} idioma {generated_language.name}'],
-                                 "instructions": ['¡Prueba de traducir lo que quieras! Puedes incluso traducir poesía.',
-                                                  'Si te gusta el idioma, puedes guardarlo como un archivo. Luego puedes simplemente cargarlo en la ventana de traducir.'],
+                "new_language":  {  "title":        [f'Observa el {languaje_adjective} idioma {generated_language.name}'],
+                                    "instructions": ['¡Prueba de traducir lo que quieras! Puedes incluso traducir poesía.',
+                                                    'Si te gusta el idioma, puedes guardarlo como un archivo. Luego puedes simplemente cargarlo en la ventana de traducir.'],
                                  
-                                 "test_poetry": ['Había una vez una vaca',
-                                                'en la Quebrada de Humahuaca.',
-                                                'Como era muy vieja, muy vieja,',
-                                                'estaba sorda de una oreja.',
-                                                ' ',
-                                                'Y a pesar de que ya era abuela',
-                                                'un día quiso ir a la escuela.',
-                                                'Se puso unos zapatos rojos,',
-                                                'guantes de tul y un par de anteojos.',
-                                                ' ',
-                                                'La vio la maestra asustada',
-                                                'y dijo: Estas equivocada.',
-                                                'Y la vaca le respondió:',
-                                                '¿Por qué no puedo estudiar yo?',],
+                                    "test_poetry":  ['Había una vez una vaca',
+                                                    'en la Quebrada de Humahuaca.',
+                                                    'Como era muy vieja, muy vieja,',
+                                                    'estaba sorda de una oreja.',
+                                                    ' ',
+                                                    'Y a pesar de que ya era abuela',
+                                                    'un día quiso ir a la escuela.',
+                                                    'Se puso unos zapatos rojos,',
+                                                    'guantes de tul y un par de anteojos.',
+                                                    ' ',
+                                                    'La vio la maestra asustada',
+                                                    'y dijo: Estas equivocada.',
+                                                    'Y la vaca le respondió:',
+                                                    '¿Por qué no puedo estudiar yo?',],
 
-                                 "new_button": ["Nuevo Idioma"],
-                                 "save_button": ["Guardar como Archivo"],
+                                    "new_button": ["Nuevo Idioma"],
+                                    "save_button": ["Guardar como Archivo"],
 
-                                 },
-                                 
+                                    },
+                "translator":  {    "title":        [f'Estás traduciendo el idioma {loaded_language.name}'],
+                                    "instructions": ['Carga un idioma de un archivo .slang (Puedes generar uno desde la pestaña de «Nuevo»)',
+                                                     '',
+                                                    f'¡Prueba de traducir lo que quieras de {loaded_language.name} a español o viceversa! (Aunque la traducción de nuevo a español está en proceso de desarollo)',
+                                                     'Puedes incluso traducir poesía.',
+                                                     'Escribe directamente en el traductor interactivo, o traduce un archivo de texto (.txt).'],
+
+                                    "translation_direction": ["", "Dirección de traducción: "] , 
+
+                                    "nat_lang_to_gen": f"español → {loaded_language.name}",
+                                    "gen_lang_to_nat": f"{loaded_language.name} → español",
+
+                                    "load_lang_button" : "Cargar idioma",
+                                    "translate_file_button" : "Traducir archivo de texto (.txt)",
+                                    "text_hint": ["Escribe aquí..."],
+
+                                    },
+                "file":     {       "translated_to": "traducido_a",
+                                    "nat_lang_name": "español"
+
+                                    }               
                                 }
+                
